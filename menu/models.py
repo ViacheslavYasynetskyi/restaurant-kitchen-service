84a310c6bbc69f0ec7c5599d96a9a80c3ec219ea
Viacheslav Yasynetskyi <slawa1594@gmail.com>
2023-07-20 00:24:49 +0100

    Add model to admin panel, change class Cook in models.py

--- menu/models.py
+++ menu/models.py
@@ -9,9 +9,12 @@ class DishType(models.Model):
     class Meta:
         ordering = ["name"]
 
+    def __str__(self):
+        return self.name
+
 
 class Cook(AbstractUser):
-    years_of_experience = models.IntegerField()
+    years_of_experience = models.IntegerField(default=0)
 
     class Meta:
         verbose_name = "cook"
