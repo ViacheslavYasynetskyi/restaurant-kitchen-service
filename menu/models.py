d294728aa1053ee3dc735d572814f4fdd604eeab
Viacheslav Yasynetskyi <slawa1594@gmail.com>
2023-07-19 22:27:02 +0100

    Add models DishType, Cook, Dish in models.py and create first migrate

--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,35 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class DishType(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+
+class Cook(AbstractUser):
+    years_of_experience = models.IntegerField()
+
+    class Meta:
+        verbose_name = "cook"
+        verbose_name_plural = "cooks"
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    dish_type = models.ForeignKey(
+        DishType,
+        on_delete=models.CASCADE
+    )
+    cooks = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name="cooks"
+    )
+
+    class Meta:
+        ordering = ["dish_type"]
