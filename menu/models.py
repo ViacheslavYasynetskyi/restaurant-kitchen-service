045ffe592f0e30cc2a07cc5f2281c289ebffee98
Viacheslav Yasynetskyi <slawa1594@gmail.com>
2023-08-01 22:55:07 +0100

    add tests to menu

--- menu/models.py
+++ menu/models.py
@@ -25,7 +25,7 @@ class Cook(AbstractUser):
 class Dish(models.Model):
     name = models.CharField(max_length=255, unique=True)
     description = models.TextField()
-    price = models.DecimalField(max_digits=5, decimal_places=2)
+    price = models.DecimalField(max_digits=5, decimal_places=2, unique=True)
     dish_type = models.ForeignKey(
         DishType,
         on_delete=models.CASCADE
